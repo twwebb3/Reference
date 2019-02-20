2783786b0ee7d5a9eeef5cb31312fe561c03fa2c
T W Webb <TW.Webb@ilg.com>
2019-02-20 15:23:57 -0500

    V2 of Phrase Creator.

--- NLP/sentimentAnalysis/phraseCreator.R
+++ NLP/sentimentAnalysis/phraseCreator.R
@@ -1,14 +1,22 @@
+#Updates: Allows user to exclude stop words (i.e. 'I', 'we', 'is', etc.)
 #This function extracts phrases from comments.
 #One simply passes a data frame and the name of the column with comments to be parsed into phrases.
 #Words, or "tokens", are returned from the comments as well.
 
-phraseCreator<-function(df,commentCol){
+phraseCreator<-function(df,commentCol,removeStopWords=FALSE){
   library(dplyr)
   library(tidytext)
   
   df$key<-1:length(df[[commentCol]])
   
   tidy_df<-unnest_tokens_(df,"word",commentCol)
+  
+  if(removeStopWords)
+  {
+    x=1
+    data("stop_words")
+    tidy_df<-tidy_df %>% anti_join(stop_words,by=c("word"="word"))
+  }
   
   tidy_df$key2<-1:length(tidy_df$word)
   temp<-select(tidy_df,key,key2,word)
