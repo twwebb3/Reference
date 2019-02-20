2116e8b71d3f2016ad061e240ad3f816d5b6603e
T W Webb <TW.Webb@ilg.com>
2019-02-20 15:25:17 -0500

    Signed-off-by: T W Webb <twwebb1@me.com>

--- NLP/sentimentAnalysis/phraseCreator.R
+++ NLP/sentimentAnalysis/phraseCreator.R
@@ -2,12 +2,13 @@
 #This function extracts phrases from comments.
 #One simply passes a data frame and the name of the column with comments to be parsed into phrases.
 #Words, or "tokens", are returned from the comments as well.
+#Allows up phrase length of up to 3 words.
 
-phraseCreator<-function(df,commentCol,removeStopWords=FALSE){
+phraseCreator<-function(df,commentCol,removeStopWords=FALSE,phraseLength=2){
   library(dplyr)
   library(tidytext)
   
-  df$key<-1:length(df[[commentCol]])
+  df$temporaryKey<-1:length(df[[commentCol]])
   
   tidy_df<-unnest_tokens_(df,"word",commentCol)
   
@@ -18,15 +19,25 @@ phraseCreator<-function(df,commentCol,removeStopWords=FALSE){
     tidy_df<-tidy_df %>% anti_join(stop_words,by=c("word"="word"))
   }
   
-  tidy_df$key2<-1:length(tidy_df$word)
-  temp<-select(tidy_df,key,key2,word)
-  temp$key2<-temp$key2-1
+  tidy_df$temporaryKey2<-1:length(tidy_df$word)
+  temp<-select(tidy_df,temporaryKey,temporaryKey2,word)
+  temp$temporaryKey2<-temp$temporaryKey2-1
   
-  out<-left_join(tidy_df,temp,by=c("key"="key","key2"="key2"))
+  out<-left_join(tidy_df,temp,by=c("temporaryKey"="temporaryKey","temporaryKey2"="temporaryKey2"))
   
-  out$phrase<-paste(out$word.x,out$word.y,sep=" ")
-  out$key<-NULL
-  out$key2<-NULL
+  if(phraseLength==3)
+  {
+    temp$temporaryKey2=temp$temporaryKey2-1
+    out<-left_join(out,temp,by=c("temporaryKey"="temporaryKey","temporaryKey2"="temporaryKey2"))
+    out$phrase<-paste(out$word.x,out$word.y,out$word,sep=" ")
+  }else
+  {
+    out$phrase<-paste(out$word.x,out$word.y,sep=" ")
+  }
+  
+  
+  out$temporaryKey<-NULL
+  out$temporaryKey2<-NULL
   
   return(out)
 }
